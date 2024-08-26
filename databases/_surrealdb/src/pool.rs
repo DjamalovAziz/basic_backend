0467a71a304f5ba587df6b17d3664bad213d6c90
Aziz <djamalov.aziz.mansurovich@gmail.com>
2024-08-26 15:34:40 +0500

    0.1.4

--- databases/_surrealdb/src/pool.rs
+++ databases/_surrealdb/src/pool.rs
@@ -12,19 +12,19 @@ pub async fn surrealdb_pool() -> surrealdb::Result<Surreal<Client>> {
     dotenv().ok();
 
     info!("Connecting surrealdb...");
-    let surrealdb = Surreal::new::<Ws>(get_env_or("SURREALDB_LOCATION", "127.0.0.1:7002")).await?;
+    let surrealdb = Surreal::new::<Ws>(get_env_or("SURREALDB_ADDRESS", "127.0.0.1:7002")).await?;
 
     info!("Signing in...");
     surrealdb
         .signin(Root {
-            username: get_env_or("SURREALDB_NAME", "root").as_str(),
-            password: get_env_or("SURREALDB_PASSWORD", "root").as_str(),
+            username: get_env_or("SURREALDB_USERNAME", "surrealdb_username").as_str(),
+            password: get_env_or("SURREALDB_PASSWORD", "surrealdb_password").as_str(),
         })
         .await?;
 
     surrealdb
-        .use_ns(get_env_or("SURREALDB_NAMESPACE", "basic_surrealdb"))
-        .use_db(get_env_or("SURREALDB_DATABASE", "basic_surrealdb"))
+        .use_ns(get_env_or("SURREALDB_NAMESPACE", "surrealdb_namespace"))
+        .use_db(get_env_or("SURREALDB_DATABASE", "surrealdb_database"))
         .await?;
     println!("[✅] Surrealdb connected successfully!");
     Ok(surrealdb)
